d80c1b4f1dbf7655b12d222946017e699ffa8152
Meth98 <105994103+Meth98@users.noreply.github.com>
2025-12-02 23:20:26 +0100

    Fix message for no IP addresses found

--- GetIPAddress.ps1
+++ GetIPAddress.ps1
@@ -7,12 +7,12 @@
 # get the IP address info (preferred = IP configuration info for addresses that are valid and available for use)
 $ip_address = Get-NetIPAddress -AddressState Preferred -AddressFamily IPv4,IPv6 | Format-Table IPAddress,AddressFamily,PrefixOrigin,SuffixOrigin
 
-# check the number of IP addresses
 if ($ip_address.Count -gt 1) {
         Write-Host -ForegroundColor red "`nMultiple IP addresses found!"
         $ip_address
 } elseif ($ip_address.Count -eq 0) {
-        Write-Host -ForegroundColor red "No IP addresses found!"
+        Write-Host -ForegroundColor red "No IP address found!"
 } else {
         $ip_address
+
 }
